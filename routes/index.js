b1cd955787f196808f78c3ee6d8d419d30223c9e
K-Rohini <rohinichinni2001@gmail.com>
2021-09-15 00:44:35 +0530

    added root get message

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
 
-router.get('/',(req,res)=>"<div> https://github.com/K-Rohini/ecommerce_api.git </div>")
+router.get('/',(req,res)=>res.send("<div>https://github.com/K-Rohini/ecommerce_api</div>"))
 
 router.use("/api",require("./api"));
 
